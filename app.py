19e9368d5e46d9f3db22faaf9e0f37ae8a8554d3
Karl Kablisk <karlkablisk@gmail.com>
2023-07-25 17:28:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,103 @@
 import streamlit as st
 import json
 import os
-import elevenlabs
 from elevenlabs import generate, play, voices
+from typing import List
 
-def split_text(text, limit=400):
-    words = text.split()
-    chunks = []
-    current_chunk = ''
-
-    for word in words:
-        if len(current_chunk) + len(word) <= limit:
-            current_chunk += ' ' + word
-        else:
-            chunks.append(current_chunk.strip())
-            current_chunk = word
-    if current_chunk:
-        chunks.append(current_chunk.strip())
-
-    return chunks
-
-def save_voicelist(voice_list, filename="11voicelist.json"):
-    with open(filename, "w") as f:
-        json.dump(voice_list, f)
-
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def load_voicelist(filename="11voicelist.json"):
+# Function to load API keys from a file
+def load_api_keys_from_file() -> List[str]:
     try:
-        if os.path.exists(filename):
-            with open(filename, "r") as f:
-                return json.load(f)
-        else:
-            voice_list = voices()
-            save_voicelist(voice_list)
-            return voice_list
-    except (json.JSONDecodeError, FileNotFoundError):
-        st.warning("There was an issue loading the voice list. Defaulting to 'Bella'.")
-        return ["Bella"]
+        with open("api_keys.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return [""] * 5
 
-def get_audio_with_key(api_key, text, voice="Bella"):
-    elevenlabs.api_key = api_key
-    return generate(
-        text=text,
-        voice=voice,
-        model="eleven_monolingual_v1"
-    )
+# Function to save API keys to a file
+def save_api_keys_to_file(api_keys: List[str]):
+    with open("api_keys.json", "w") as f:
+        json.dump(api_keys, f)
 
+# Function to generate audio
+def get_audio(text, voice="Bella", model="eleven_monolingual_v1", api_key=None):
+    try:
+        audio = generate(text=text, voice=voice, model=model, api_key=api_key)
+        return audio
+    except Exception as e:
+        st.warning(f"Error: {str(e)}")
+        return None
+
+# Display app title
 st.title('ElevenLabs Audio Generator')
 
+# Load API keys either from file or start with empty ones
+if 'streamlit' not in os.getcwd() and os.path.exists("api_keys.json"):
+    initial_api_keys = load_api_keys_from_file()
+else:
+    initial_api_keys = [""] * 5
+
 # Sidebar for API key input
-api_keys = [st.sidebar.text_input(f"API Key {i+1}") for i in range(5)]
+api_key_labels = [f"API Key {i+1}" for i in range(5)]
+api_keys = [st.sidebar.text_input(label, value=initial_api_keys[i]) for i, label in enumerate(api_key_labels)]
 marked_keys = st.session_state.get("marked_keys", [False]*5)
 
-# Load or fetch voices and then display the dropdown
-voice_list = load_voicelist()
+# Save API keys if not on Streamlit's cloud
+if 'streamlit' not in os.getcwd():
+    save_api_keys_to_file(api_keys)
+
+# Manual API key selection
+options = ["NONE"] + [f"API Key {i+1}" for i in range(5)]
+selected_api_option = st.sidebar.selectbox("Manually select an API Key", options, index=0)
+
+# Model selection dropdown
+model_mapping = {
+    'monolingual': 'eleven_monolingual_v1',
+    'multilingual': 'eleven_multilingual_v1'
+}
+selected_model_name = st.selectbox("Select a model:", list(model_mapping.keys()))
+selected_model = model_mapping[selected_model_name]
+
+# Display the dropdown for voices
+voice_list = ["Rachel", "Domi", "Bella", "Antoni", "Elli", "Josh", "Arnold", "Adam", "Sam"]
 selected_voice = st.selectbox('Select a voice:', voice_list)
 
 user_input = st.text_area('Enter/Paste your text here:', height=200)
 
-if user_input:
+used_api_key = "NONE"  # Default as NONE
+
+if st.button('SPEAK') and user_input:
     generated = False
-    for idx, api_key in enumerate(api_keys):
-        if api_key and not marked_keys[idx]:
-            try:
-                audio = get_audio_with_key(api_key, user_input, selected_voice)
-                st.audio(audio, format='audio/wav')
+
+    # Use manually selected API key if it's valid and not "NONE"
+    if selected_api_option != "NONE":
+        api_idx = options.index(selected_api_option) - 1
+        if api_keys[api_idx] and not marked_keys[api_idx]:
+            audio = get_audio(user_input, selected_voice, selected_model, api_keys[api_idx])
+            if audio:
+                st.audio(audio, format='audio/wav', autoplay=True)
                 generated = True
-                break
-            except:
-                marked_keys[idx] = True
-                st.sidebar.markdown(f"API Key {idx+1} is marked as full.")
-    
-    # If no valid API keys, or they all failed
+                used_api_key = selected_api_option
+            else:
+                marked_keys[api_idx] = True
+
+    # If manually selected API key failed or wasn't valid, or if "NONE" was selected, try the rest
     if not generated:
-        # Splitting the input into 400 character chunks
-        text_chunks = split_text(user_input)
+        for idx, api_key in enumerate(api_keys):
+            if api_key and not marked_keys[idx]:
+                audio = get_audio(user_input, selected_voice, selected_model, api_key)
+                if audio:
+                    st.audio(audio, format='audio/wav', autoplay=True)
+                    generated = True
+                    used_api_key = f"API Key {idx+1}"
+                    break
+                else:
+                    marked_keys[idx] = True
 
-        for chunk in text_chunks:
-            audio = get_audio(chunk, selected_voice)
-            st.audio(audio, format='audio/wav')
+    # If no valid API keys or they all failed
+    if not generated:
+        st.warning("No API key provided or all provided keys are exhausted. Cannot generate audio.")
 
-st.session_state.marked_keys = marked_keys
+    # Print the API key that was used
+    st.write(f"Audio generated using: {used_api_key}")
+
+# Store marked keys to session state
+st.session_state["marked_keys"] = marked_keys
