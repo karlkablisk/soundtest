7660e37774b4c5313f8fbdb40db05e275c9e2b6d
Karl Kablisk <karlkablisk@gmail.com>
2023-07-25 17:27:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import json
+import os
 import elevenlabs
-from elevenlabs import generate, voices
+from elevenlabs import generate, play, voices
 
 def split_text(text, limit=400):
     words = text.split()
@@ -18,95 +20,64 @@ def split_text(text, limit=400):
 
     return chunks
 
-def fetch_voicelist():
-    try:
-        return voices()
-    except:
-        st.sidebar.warning("There was an issue loading the voice list. Defaulting to predefined list.")
-        return ["Rachel", "Domi", "Bella", "Antoni", "Elli", "Josh", "Arnold", "Adam", "Sam"]
+def save_voicelist(voice_list, filename="11voicelist.json"):
+    with open(filename, "w") as f:
+        json.dump(voice_list, f)
 
-def get_audio(text, voice="Bella", model="eleven_monolingual_v1", api_key=None):
-    if api_key:
-        elevenlabs.api_key = api_key
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def load_voicelist(filename="11voicelist.json"):
+    try:
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                return json.load(f)
+        else:
+            voice_list = voices()
+            save_voicelist(voice_list)
+            return voice_list
+    except (json.JSONDecodeError, FileNotFoundError):
+        st.warning("There was an issue loading the voice list. Defaulting to 'Bella'.")
+        return ["Bella"]
+
+def get_audio_with_key(api_key, text, voice="Bella"):
+    elevenlabs.api_key = api_key
     return generate(
         text=text,
         voice=voice,
-        model=model
+        model="eleven_monolingual_v1"
     )
 
 st.title('ElevenLabs Audio Generator')
 
 # Sidebar for API key input
-api_key_labels = [f"API Key {i+1}" for i in range(5)]
-api_keys = [st.sidebar.text_input(label) for label in api_key_labels]
+api_keys = [st.sidebar.text_input(f"API Key {i+1}") for i in range(5)]
 marked_keys = st.session_state.get("marked_keys", [False]*5)
 
-# Display marked API keys
-for idx, marked in enumerate(marked_keys):
-    if marked:
-        st.sidebar.markdown(f"<span style='color:red'>x</span> API Key {idx+1} is marked as full.", unsafe_allow_html=True)
-
-# Button to fetch new voice list
-if st.sidebar.button('Fetch New Voice List'):
-    voice_list = fetch_voicelist()
-else:
-    voice_list = ["Rachel", "Domi", "Bella", "Antoni", "Elli", "Josh", "Arnold", "Adam", "Sam"]
-
-# Manual API key selection
-options = ["NONE"] + [f"API Key {i+1}" for i in range(5)]
-selected_api_option = st.sidebar.selectbox("Manually select an API Key", options, index=0)
-
-# Model selection dropdown
-model_mapping = {
-    'monolingual': 'eleven_monolingual_v1',
-    'multilingual': 'eleven_multilingual_v1'
-}
-selected_model_name = st.selectbox("Select a model:", list(model_mapping.keys()))
-selected_model = model_mapping[selected_model_name]
-
-# Display the dropdown for voices
+# Load or fetch voices and then display the dropdown
+voice_list = load_voicelist()
 selected_voice = st.selectbox('Select a voice:', voice_list)
 
 user_input = st.text_area('Enter/Paste your text here:', height=200)
 
-# Add a button for generating audio
-if st.button('SPEAK') and user_input:
+if user_input:
     generated = False
-
-    # Use manually selected API key if it's valid and not "NONE"
-    if selected_api_option != "NONE":
-        api_idx = options.index(selected_api_option) - 1
-        if api_keys[api_idx] and not marked_keys[api_idx]:
+    for idx, api_key in enumerate(api_keys):
+        if api_key and not marked_keys[idx]:
             try:
-                audio = get_audio(user_input, selected_voice, selected_model, api_keys[api_idx])
+                audio = get_audio_with_key(api_key, user_input, selected_voice)
                 st.audio(audio, format='audio/wav')
                 generated = True
+                break
             except:
-                marked_keys[api_idx] = True
-
-    # If manually selected API key failed or wasn't valid, or if "NONE" was selected, try the rest
+                marked_keys[idx] = True
+                st.sidebar.markdown(f"API Key {idx+1} is marked as full.")
+    
+    # If no valid API keys, or they all failed
     if not generated:
-        for idx, api_key in enumerate(api_keys):
-            if api_key and not marked_keys[idx]:
-                try:
-                    audio = get_audio(user_input, selected_voice, selected_model, api_key)
-                    st.audio(audio, format='audio/wav')
-                    generated = True
-                    break
-                except:
-                    marked_keys[idx] = True
+        # Splitting the input into 400 character chunks
+        text_chunks = split_text(user_input)
 
-    # If all provided API keys failed or "NONE" was selected, try without an API key
-    if not generated:
-        try:
-            audio = get_audio(user_input, selected_voice, selected_model)
+        for chunk in text_chunks:
+            audio = get_audio(chunk, selected_voice)
             st.audio(audio, format='audio/wav')
-            generated = True
-        except:
-            pass
-
-    # If audio still not generated after all attempts
-    if not generated:
-        st.warning("No API key provided or all provided keys are exhausted. Cannot generate audio.")
 
 st.session_state.marked_keys = marked_keys
